6003debb12243a1fc032ac8f2258330aa4990855
Dishant Kushwaha <dishantkushwaha71@gmail.com>
2025-07-31 09:33:46 +0530

    Created config service for config server

---
+++ src/test/java/com/saas/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saas.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
